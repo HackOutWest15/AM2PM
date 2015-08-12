c09027d76e8207a80de3a9906d0c902ecfb70cc4
limbero <samuelsson.axel@gmail.com>
2015-08-12 14:21:51 +0200

    grejer

--- client/script.js
+++ client/script.js
@@ -1,21 +1,58 @@
-document.querySelector('a.artist.card').addEventListener('click', function() {
+$('a.artist.card').on('click', function() {
     if(!dragCheck) {
         document.querySelector('#swipeview').classList.add('hiddenleft');
         document.querySelector('#artistview').classList.remove('hiddenright');
     }
 });
-document.querySelector('a.navback').addEventListener('click', function() {
+$('a.navback').on('click', function() {
     document.querySelector('#swipeview').classList.remove('hiddenleft');
     document.querySelector('#artistview').classList.add('hiddenright');
 });
+$('a.getschedule').on('click', function() {
+    var request = new XMLHttpRequest();
+    request.open('POST', 'http://10.47.12.119:5000/schedule', true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.onload = receivedSchedule;
+    request.send(JSON.stringify(liked));
+});
+
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
+
+function receivedSchedule() {
+    audio.pause();
+    $('#scheduleview .main').html('');
+    var shows = JSON.parse(this.responseText);
+    var lastdate = new Date('1992-10-12');
+    for(var i=0; i < shows.length; i++) {
+        var date = new Date(shows[i].start_date);
+        if(parseInt(shows[i].start_time.substr(0,2)) < 8) {
+            date.setDate(date.getDate()-1);
+        }
+        if(date > lastdate) {
+            lastdate = date;
+            $('#scheduleview .main').append('<div class="datemarker"><p>'+days[date.getDay()]+'</p></div>');
+        }
+
+        $('#scheduleview .main').append('<div class="concert"><p>'+shows[i].name+'<br>'+shows[i].venue+' '+shows[i].start_time+'</p></div>');
+    }
+    document.querySelector('#scheduleview').classList.remove('hiddenbottom');
+}
 
 function requestedResponse () {
     data = JSON.parse(this.responseText);
+    shuffle(data);
+
     localStorage.setItem('data', this.responseText);
     setArtist(currentArtist);
 }
 
 function setArtist(cur) {
+    if(cur === 2) {
+        $('.firstinfo').addClass('hidden');
+    }
     if(data.length === 0) return;
 
     var els = document.querySelectorAll('.totalartistsnum');
@@ -35,7 +72,18 @@ function setArtist(cur) {
 
     els = document.querySelectorAll('.artisturl');
     [].forEach.call(els, function(el) {
-        el.href = data[cur-1].artist_page_url;
+        if(typeof data[cur-1].artist_page_url !== 'undefined') {
+            el.href = data[cur-1].artist_page_url;
+            $(el).removeClass('hidden');
+            $('a.artist.card').off().on('click', function() {
+                if(!dragCheck) {
+                    document.querySelector('#swipeview').classList.add('hiddenleft');
+                    document.querySelector('#artistview').classList.remove('hiddenright');
+                }
+            });
+        } else {
+            $('a.artist.card').off().on('click', function() {});
+        }
     });
 
     els = document.querySelectorAll('.artist.card .photo');
@@ -53,6 +101,25 @@ function setArtist(cur) {
 
 function setTrack(cur) {
     if(typeof data[currentArtist-1].tracks === 'undefined') {
+        els = document.querySelectorAll('.trackname');
+        [].forEach.call(els, function(el) {
+            el.innerHTML = '';
+        });
+
+        els = document.querySelectorAll('.albumname');
+        [].forEach.call(els, function(el) {
+            el.innerHTML = '';
+        });
+
+        els = document.querySelectorAll('.album.card .photo');
+        [].forEach.call(els, function(el) {
+            el.style.backgroundImage = "url('beck.jpg')";
+        });
+
+        audio.src = '';
+        audio.load();
+        audio.play();
+
         return;
     }
 
@@ -71,11 +138,28 @@ function setTrack(cur) {
         el.style.backgroundImage = "url('"+data[currentArtist-1].tracks[currentTrack-1].album.image_url+"')";
     });
 
-    audio = document.querySelector('audio');
     audio.src = data[currentArtist-1].tracks[currentTrack-1].preview_url;
     audio.load();
     audio.play();
 }
+
+function displayBadge(bool, callback) {
+    if(bool) {
+        document.querySelector('.badge').style.backgroundImage = "url('PopAdd.png')";
+    } else {
+        document.querySelector('.badge').style.backgroundImage = "url('PopDontAdd.png')";
+    }
+    $('.badge.invisible').removeClass('invisible');
+    $('.badge.hidden').removeClass('hidden');
+    setTimeout(function() {
+        $('.badge').addClass('hidden');
+    }, 1400);
+    setTimeout(function() {
+        $('.badge').addClass('invisible');
+    }, 1050);
+}
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 var data = JSON.parse(localStorage.getItem('data')) || [];
 var liked = JSON.parse(localStorage.getItem('liked')) || [];
@@ -83,12 +167,18 @@ var currentArtist = localStorage.getItem('currentArtist') || 1;
 var currentTrack = 1;
 var dragCheck = false;
 
+var audio = document.querySelector('audio');
+
+if(currentArtist > 1) { $('.firstinfo').addClass('hidden'); }
+
 setArtist(currentArtist);
 
-var request = new XMLHttpRequest();
-request.onload = requestedResponse;
-request.open('get', 'http://10.47.12.119:5000/artists', true);
-request.send();
+if(localStorage.getItem('data') === null) {
+    var request = new XMLHttpRequest();
+    request.onload = requestedResponse;
+    request.open('GET', 'http://10.47.12.119:5000/artists', true);
+    request.send();
+}
 
 $(function() {
     $('.draggable').draggable({
@@ -106,19 +196,50 @@ $(function() {
         activeClass: 'ui-state-default',
         hoverClass: 'ui-state-hover',
         drop: function(event, ui) {
+            this.classList.add('ui-state-hover');
             if(this.classList.contains('yes')) {
-                liked.push({'id': data[currentArtist-1].name});
+                liked.push(data[currentArtist-1].name || '');
                 localStorage.setItem('liked', JSON.stringify(liked));
+                
+                ui.draggable.parent().addClass('hidden', function() {
+                    setTimeout(function() { $('.ui-state-hover').removeClass('ui-state-hover'); }, 150);
+                    setTimeout(function() {
+                        $('.card.artist .photo').removeAttr('style');
+                        if(currentArtist < data.length) {
+                            setArtist(++currentArtist);
+                        } else {
+                            var request = new XMLHttpRequest();
+                            request.open('POST', 'http://10.47.12.119:5000/schedule', true);
+                            request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+                            request.onload = receivedSchedule;
+                            request.send(JSON.stringify(liked));
+                        }
+                        $('.card.artist.hidden').removeClass('hidden');
 
-                ui.draggable.removeAttr('style');
-                if(currentArtist < data.length) {
-                    setArtist(++currentArtist);
-                }
+                    }, 350);
+
+                    displayBadge(true);
+                });
             } else {
-                ui.draggable.removeAttr('style');
-                if(currentArtist < data.length) {
-                    setArtist(++currentArtist);
-                }
+
+                ui.draggable.parent().addClass('hidden', function() {
+                    setTimeout(function() { $('.ui-state-hover').removeClass('ui-state-hover'); }, 150);
+                    setTimeout(function() {
+                        $('.card.artist .photo').removeAttr('style');
+                        if(currentArtist < data.length) {
+                            setArtist(++currentArtist);
+                        } else {
+                            var request = new XMLHttpRequest();
+                            request.open('POST', 'http://10.47.12.119:5000/schedule', true);
+                            request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+                            request.onload = receivedSchedule;
+                            request.send(JSON.stringify(liked));
+                        }
+                        $('.card.artist.hidden').removeClass('hidden');
+                    }, 350);
+
+                    displayBadge(false);
+                });
             }
         }
     });
